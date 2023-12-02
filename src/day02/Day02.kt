37994a046ce93b58b7310121733b8a813bea48ad
npace <lubonay@gmail.com>
2023-12-02 23:41:52 +0200

    Solve part 2 of day 2

--- src/day02/Day02.kt
+++ src/day02/Day02.kt
@@ -9,32 +9,50 @@ fun main() {
     assertEqual(part1(testInput1), 8)
     val input = readInput("day02/Day02")
     log(part1(input))
+
+    assertEqual(part2(testInput1), 2286)
+    log(part2(input))
 }
 
-const val MAX_RED = 12
-const val MAX_GREEN = 13
-const val MAX_BLUE = 14
+enum class Color(val colorName: String, val max: Int) {
+    Red("red", 12),
+    Green("green", 13),
+    Blue("blue", 14);
+}
 
-val maxAmountByColor = mapOf(
-    "red" to MAX_RED,
-    "green" to MAX_GREEN,
-    "blue" to MAX_BLUE,
-)
+fun String.toColor(): Color {
+    return Color.entries.first { it.colorName == this }
+}
+
 data class Game(
     val id: Int,
     val isPossible: Boolean,
-)
+    val minRed: Int,
+    val minGreen: Int,
+    val minBlue: Int,
+) {
+    fun power(): Int {
+        return minRed * minGreen * minBlue
+    }
+}
 
 fun part1(input: List<String>): Int {
     val games = input.map { it.parseGame() }
     log(games.map { it.toString() + it.isPossible })
-    return games.sumOf { if (it.isPossible) it.id else 0 }
+    return games.filter { it.isPossible }.sumOf { it.id }
+}
+
+fun part2(input: List<String>): Int {
+    val games = input.map { it.parseGame() }
+    log(games)
+    return games.sumOf { it.power() }
 }
 
 private fun String.parseGame(): Game {
     log(this)
     val id = parseId()
     var isPossible = true
+    val mins = Color.entries.associateWith { 0 }.toMutableMap()
     val sets = removeRange(0, indexOf(": ") + 2)
         .split("; ")
     sets.forEach { set ->
@@ -42,15 +60,19 @@ private fun String.parseGame(): Game {
         log("cubes  $cubes")
         cubes.forEach { cube ->
             val amount = cube.substringBefore(" ").toInt()
-            val color = cube.substringAfter(" ")
+            val color = cube.substringAfter(" ").toColor()
             log("cube    $cube: amount: $amount, color: $color")
-            if (amount > maxAmountByColor[color]!!) {
+            if (amount > color.max) {
                 isPossible = false
+            }
+            val currentMin = mins[color]!!
+            if (amount > currentMin) {
+                mins[color] = amount
             }
         }
     }
 
-    return Game(id, isPossible)
+    return Game(id, isPossible, mins[Color.Red]!!, mins[Color.Green]!!, mins[Color.Blue]!!)
 }
 
 private fun String.parseId(): Int {
