b625efc3727ceab53a202f7aaa7fd4b09b9e1fa3
npace <lubonay@gmail.com>
2023-12-06 21:14:49 +0200

    Solve part 2 of day 6

--- src/day06/Day06.kt
+++ src/day06/Day06.kt
@@ -8,9 +8,13 @@ fun main() {
     val testInput1 = readInput("day06/Day06_test")
     val input = readInput("day06/Day06")
 
-    val startTime = System.currentTimeMillis()
+    var startTime = System.currentTimeMillis()
     assertEqual(part1(testInput1), 288)
     log("Part 1 result: ${part1(input)}, took ${System.currentTimeMillis() - startTime}ms")
+
+    startTime = System.currentTimeMillis()
+    assertEqual(part2(testInput1), 71503)
+    log("Part 2 result: ${part2(input)}, took ${System.currentTimeMillis() - startTime}ms")
 }
 
 private fun part1(input: List<String>): Int {
@@ -20,17 +24,31 @@ private fun part1(input: List<String>): Int {
     return times
         .mapIndexed { index, time ->
             countOfRecordBreakingDistances(time, recordDistances[index])
-        }.fold(1) { acc, i -> i * acc }
+        }.marginOfError()
+}
+
+private fun List<Int>.marginOfError() = fold(1) { acc, i -> i * acc }
+
+private fun part2(input: List<String>): Int {
+    log(input)
+    val (time, recordDistance) = getTimeAndRecordDistance(input)
+    return countOfRecordBreakingDistances(time, recordDistance)
 }
 
 private fun getTimesAndRecordDistances(input: List<String>) = input.map { line ->
     line.substringAfter(":")
         .split(" ")
         .filter { it.isNotBlank() }
-        .map { it.toInt() }
+        .map { it.toLong() }
 }
 
-private fun countOfRecordBreakingDistances(time: Int, recordDistance: Int): Int {
+private fun getTimeAndRecordDistance(input: List<String>) = input.map { line ->
+    line.substringAfter(":")
+        .replace(" ", "")
+        .toLong()
+}
+
+private fun countOfRecordBreakingDistances(time: Long, recordDistance: Long): Int {
     val distances = (0..<time).map { t ->
         val remainingTime = time - t
         val speedPerSecond = 1 * t
