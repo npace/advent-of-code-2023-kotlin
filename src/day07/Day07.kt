8a4e8f0ce6fbbc811c0d12d695edbb9f8c6a332e
npace <lubonay@gmail.com>
2023-12-07 22:56:38 +0200

    Solve part 2 of day 7

--- src/day07/Day07.kt
+++ src/day07/Day07.kt
@@ -1,6 +1,7 @@
 package day07
 
 import assertEqual
+import day07.Card.J
 import log
 import readInput
 
@@ -8,34 +9,53 @@ fun main() {
     val testInput1 = readInput("day07/Day07_test")
     val input = readInput("day07/Day07")
 
-    val startTime = System.currentTimeMillis()
+    var startTime = System.currentTimeMillis()
     assertEqual(part1(testInput1), 6440)
     log("Part 1 result: ${part1(input)}, took ${System.currentTimeMillis() - startTime}ms")
+
+    startTime = System.currentTimeMillis()
+    assertEqual(part2(testInput1), 5905)
+    log("Part 2 result: ${part2(input)}, took ${System.currentTimeMillis() - startTime}ms")
 }
 
 private fun part1(input: List<String>): Int {
-    log(input)
-    val hands = input.map { line ->
-        val cards = line.substringBefore(" ").map { Card.valueOf(it.toString()) }
-        val bid = line.substringAfter(" ").toInt()
-        Hand(
-            cards = cards,
-            bid = bid,
-            strength = calculateHandStrength(cards),
-        )
-    }
-    val rankedHands = rankCards(hands)
-    log("ranked hands: ${rankedHands.joinToString("\n")}")
+    val hands = parseHands(input, false)
+    val rankedHands = rankCards(hands, false)
     return calculateWinnings(rankedHands)
 }
 
-private fun calculateHandStrength(cards: List<Card>): HandStrength {
+private fun part2(input: List<String>): Int {
+    val hands = parseHands(input, true)
+    val rankedHands = rankCards(hands, true)
+    return calculateWinnings(rankedHands)
+}
+
+private fun parseHands(input: List<String>, useJoker: Boolean) = input.map { line ->
+    val cards = line.substringBefore(" ").map { Card.valueOf(it.toString()) }
+    val bid = line.substringAfter(" ").toInt()
+    Hand(
+        cards = cards,
+        bid = bid,
+        strength = calculateHandStrength(cards, useJoker),
+    )
+}
+
+private fun calculateHandStrength(cards: List<Card>, useJoker: Boolean = false): HandStrength {
     val cardFrequencyMap = mutableMapOf<Card, Int>()
     cards.forEach {
         cardFrequencyMap[it] = cardFrequencyMap.getOrDefault(it, 0) + 1
     }
-    return when {
-        cardFrequencyMap.size == 1 -> HandStrength.FiveOfAKind
+    val jokersCount = cardFrequencyMap[J]
+    if (useJoker && jokersCount != null) {
+        val mostFrequentCard = cardFrequencyMap.toList().sortedByDescending { it.second }
+            .firstOrNull { it.first != J }?.first
+        if (mostFrequentCard != null) {
+            cardFrequencyMap[mostFrequentCard] = cardFrequencyMap[mostFrequentCard]!! + jokersCount
+            cardFrequencyMap[J] = cardFrequencyMap[J]!! - jokersCount
+        }
+    }
+    val handStrength = when {
+        cardFrequencyMap.filter { it.value > 0 }.size == 1 -> HandStrength.FiveOfAKind
         cardFrequencyMap.any { it.value == 4 } -> HandStrength.FourOfAKind
         cardFrequencyMap.any { it.value == 3 } && cardFrequencyMap.any { it.value == 2 } -> HandStrength.FullHouse
         cardFrequencyMap.any { it.value == 3 } -> HandStrength.ThreeOfAKind
@@ -43,25 +63,28 @@ private fun calculateHandStrength(cards: List<Card>): HandStrength {
         cardFrequencyMap.any { it.value == 2 } -> HandStrength.OnePair
         else -> HandStrength.HighCard
     }
+    return handStrength
 }
 
-private fun rankCards(hands: List<Hand>): List<Pair<Hand, Int>> {
+private fun rankCards(hands: List<Hand>, useJoker: Boolean): List<Pair<Hand, Int>> {
     return hands
-        .sortedWith(handComparator)
+        .sortedWith(handComparator(useJoker))
         .mapIndexed { index, hand -> hand to index + 1 }
 }
 
-private val handComparator = Comparator<Hand> { o1, o2 ->
+private fun handComparator(useJoker: Boolean) = Comparator<Hand> { o1, o2 ->
     if (o1.strength > o2.strength) {
         -1
     } else if (o1.strength < o2.strength) {
         1
     } else {
+        val cardComparator = cardComparator(useJoker)
         o1.cards.forEachIndexed { index, card1 ->
             val card2 = o2.cards[index]
-            if (card1 > card2) {
+            val comparison = cardComparator.compare(card1, card2)
+            if (comparison == 1) {
                 return@Comparator -1
-            } else if (card1 < card2) {
+            } else if (comparison == -1) {
                 return@Comparator 1
             }
         }
@@ -79,6 +102,23 @@ private fun calculateWinnings(rankedCards: List<Pair<Hand, Int>>): Int {
 @Suppress("EnumEntryName")
 enum class Card {
     A, K, Q, J, T, `9`, `8`, `7`, `6`, `5`, `4`, `3`, `2`;
+
+    fun getStrength(useJoker: Boolean): Int {
+        if (useJoker) {
+            if (this == J) return entries.size - 1
+            else if (this > J) return ordinal - 1
+        }
+        return ordinal
+    }
+}
+
+private fun cardComparator(useJoker: Boolean) = Comparator<Card> { o1, o2 ->
+    if (o1.getStrength(useJoker) > o2.getStrength(useJoker))
+        1
+    else if (o1.getStrength(useJoker) < o2.getStrength(useJoker))
+        -1
+    else
+        0
 }
 
 data class Hand(val cards: List<Card>, val bid: Int, val strength: HandStrength)
